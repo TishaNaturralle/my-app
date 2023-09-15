a8575bc9c5edb3a335fe4a7f3214f3996c9cf3a5
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-09-15 23:26:46 +0200

    Added Github repo
    Cleaned app files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by {""}
+        <a href="https://www.tmaenga.za" target="_blank">
+          Ticha Maenga{""}
         </a>
-      </header>
+        and is {""}
+        <a href="https://github.com/TishaNaturralle/my-app" target="_blank">
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
