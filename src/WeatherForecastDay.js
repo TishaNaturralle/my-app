d64c012d1fd9c4b819419d13de89d6d9f1588690
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2023-09-24 13:31:18 +0200

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,39 +2,37 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+  console.log(props.data);
   function maxTemperature() {
-    console.log(props.data);
-    function maxTemperature() {
-      let temperature = Math.round(props.data.temp.maximum);
-      return `${temperature}°`;
-    }
-
-    function minTemperature() {
-      let temperature = Math.round(props.data.temp.minimum);
-      return `${temperature}°`;
-    }
-    function day() {
-      let date = new Date(props.data.dt * 1000);
-      let day = date.getDay();
-
-      let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-      return days[day];
-    }
-
-    return (
-      <div>
-        <div className="WeatherForecast-day">{day()}</div>
-        <WeatherIcon code={props.data.weather} size={36} />
-        <div className="WeatherForecast-temperatures">
-          <span className="WeatherForecast-temperature-max">
-            {maxTemperature()}
-          </span>
-          <span className="WeatherForecast-temperature-min">
-            {minTemperature()}
-          </span>
-        </div>
-      </div>
-    );
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
   }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="WeatherForecast-day">{day()}</div>
+      <WeatherIcon code={props.data.weather} size={36} />
+      <div className="WeatherForecast-temperatures">
+        <span className="WeatherForecast-temperature-max">
+          {maxTemperature()}
+        </span>
+        <span className="WeatherForecast-temperature-min">
+          {minTemperature()}
+        </span>
+      </div>
+    </div>
+  );
 }
